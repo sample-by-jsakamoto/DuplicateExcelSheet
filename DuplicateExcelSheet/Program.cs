52959a9aff3001318b43e561eee271f7e7498659
jsakamoto <junichi.jp@gmail.com>
2019-03-31 22:00:02 +0900

    Initial commit.

---
+++ DuplicateExcelSheet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+        var bookPath = Path.Combine(baseDir, "Book1.xlsx");
+
+        DuplicateExcelSheetByClosedXML(baseDir, bookPath);
+        DuplicateExcelSheetByEPPlus(baseDir, bookPath);
+    }
+
+    private static void DuplicateExcelSheetByEPPlus(string baseDir, string bookPath)
+    {
+        var outPath = Path.Combine(baseDir, "Book1 - ClosedXML.xlsx");
+        using (var book = new ClosedXML.Excel.XLWorkbook(bookPath))
+        {
+            book.Worksheet("Sheet1").CopyTo("Sheet2");
+            book.SaveAs(outPath);
+        }
+    }
+
+    private static void DuplicateExcelSheetByClosedXML(string baseDir, string bookPath)
+    {
+        var outPath = Path.Combine(baseDir, "Book1 - EPPlus.xlsx");
+        using (var package = new OfficeOpenXml.ExcelPackage(new FileInfo(bookPath)))
+        using (var book = package.Workbook)
+        {
+            book.Worksheets.Add("Sheet2", book.Worksheets["Sheet1"]);
+            package.SaveAs(new FileInfo(outPath));
+        }
+    }
+}
